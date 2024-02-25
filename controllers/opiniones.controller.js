201ec23da94973e08c7075fb03abb0d79878e3b0
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-25 10:09:53 -0600

    Controlador de opiniones

---
+++ controllers/opiniones.controller.js
@@ -0,0 +1,42 @@
+const  Opinion = require('../src/models/opiniones.model');
+const { response } = require('express');
+
+const opinionesGert = async (req, res= response) => {
+    const {limite, desde} = req.body;
+    const query = {estado: true};
+
+    const [total, opiniones] = await Promise.all([
+        Opinion.countDocuments(query),
+        Opinion.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        opiniones
+    });
+
+
+}
+
+
+const getOpinionesById = async (req, res) => {
+    const {id} = req.params;
+    const opinion = await Opinion.findOne({_id: id});
+
+    res.status(200).json({
+        opinion
+    });
+}
+
+
+const opinionesPost = async (req, res) => {
+    const {texto, calificacion, othersDet} =req.body;
+    const opinion = new Opinion({texto, calificacion, othersDet});
+
+    await opinion.save();
+    res.status(202).json({
+        opinion
+    });
+}
