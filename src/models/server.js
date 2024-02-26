2319ee78c1ac6035f77738517a8657918797bc91
Informatica <informatica@alumno.local>
2024-02-26 15:51:39 -0600

    Servidor corriendo

---
+++ src/models/server.js
@@ -0,0 +1,37 @@
+const express  = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+        this.middlewares();
+        this.router();
+
+    }
+
+    async conectarDB (){
+        await dbConnection();
+    }
+
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+
+    routes(){
+
+    }
+
+
+}
+
+
+module.exports = Server;
